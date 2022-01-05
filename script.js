06ce2391b6d42c119d000c81f475b4c0684b32e9
wiily <willyhuang18@gmail.com>
2022-01-04 16:08:23 -0800

    creating function for display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//1.make a variable for ID from Html in JS in order to manipulate it from JS
+// create variable to ID from Html in JS in order to manipulate it from JS
 var quizQuestions = document.getElementById("question");
 //same as the timer, make it as countdown
 var timer = document.querySelector(".timer");
@@ -14,7 +14,53 @@ var nextQuestions;
 var currentindex = 0;
 // make score variable equal 0 in order to add up for the final score
 var score = 0;
-// making the string variable to contain the question to be answers
+// Array of options for user to answers
 var questions = [{}];
     
 
+//onclick event causes the quiz start
+start.onclick = () =>{
+// add the css class for the quiz element to be shown
+    quiz.classList.add("activeQuiz");
+// convert questions easily comparisons
+      quizQuestions= questions[currentindex];
+      // execute the function to call on it
+        showQuestion(quizQuestions)
+// execute the time function to start countdown
+    time()
+}
+
+
+//declare time and set it into interval function  
+var times = setInterval(time, 1000)
+// countdown function for the quiz
+function time(){
+  // set the time to be 75 sec
+  var count = 75;
+    left.innerText= count + " sec left";
+         count--;
+         if (count == -1){
+          //  set the time content to empty string when it hit 0
+          left.textContent='';
+          //  use clearInterval to stop the time
+            clearInterval(times);
+        }
+
+}
+
+// display the function for the question, and giving index to initiate 
+function showQuestion(index){
+    quizQuestions.innerText = index.title;
+    // use the foreach function make sure the question choice is display
+    index.choices.forEach(element => {
+      // make sure the choice is clickable, so create a 'button' for each of them
+     var button =document.createElement("button")
+    //  attach them with css style
+    button.className="answers"
+    button.innerText=element
+    //append it to the choices
+    answers.appendChild(button)
+    //also need addEventListener to display next question
+    button.addEventListener("click", displaynextQuestion)
+  });
+}
